ad9a6d7694d5449a5734b778f9f849b242e77fb9
Antonio Marín Rodríguez <amrodriguezjarote@gmail.com>
2022-03-26 21:13:56 +0100

    Add first and second main book pages

--- pages/conte/index.js
+++ pages/conte/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import styles from 'styles/Conte.module.css'
 import UniquePageLayout from 'layouts/UniquePageLayout'
 import BookSVG from 'components/SVG/book'
+import FirstPageSVG from 'components/SVG/firstPage'
+import SecondPageSVG from 'components/SVG/secondPage'
 
 export default function Conte () {
   return (
@@ -14,23 +16,15 @@ export default function Conte () {
 
       <UniquePageLayout>
 
-        <div className={styles.bookContainer}> 
-          <BookSVG/>
-
-          <div className={styles.bookPageContainer}>
-              
-              <div className={styles.bookLeftPage}>
-                <p>d</p>
-              </div>
-
-              <div className={styles.bookRightPage}>
-              <img src='princess-sheila.svg' height={200}></img>
-              <img src='castle.svg' height={200}></img>
-              </div>
-          </div>
+      <div className={styles.bookPageContainer}>
+        <div className={styles.firstPage}>
+          <FirstPageSVG/>
         </div>
-        
-       
+
+        <div className={styles.secondPage}>
+          <SecondPageSVG/>
+        </div>      
+      </div>             
       </UniquePageLayout>
     </div>
   )
