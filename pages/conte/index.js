ce3ca90c265774ffe618ad7fd75c1296cab6f065
Antonio Marín Rodríguez <amrodriguezjarote@gmail.com>
2022-04-08 21:29:41 +0200

    Implement svg pages into page component

--- pages/conte/index.js
+++ pages/conte/index.js
@@ -4,8 +4,14 @@ import UniquePageLayout from 'layouts/UniquePageLayout'
 import BookSVG from 'components/SVG/book'
 import FirstPageSVG from 'components/SVG/firstPage'
 import SecondPageSVG from 'components/SVG/secondPage'
+import Button from 'components/Button'
+import { useState } from 'react/cjs/react.development'
 
 export default function Conte () {
+
+  const [page, setPageBook] = useState(0);
+
+
   return (
     <div className={styles.container}>
       <Head>
@@ -17,14 +23,33 @@ export default function Conte () {
       <UniquePageLayout>
 
       <div className={styles.bookPageContainer}>
-        <div className={styles.firstPage}>
-          <FirstPageSVG/>
-        </div>
+        { page === 0 && 
+          <div className={styles.bookPage} >
+            <FirstPageSVG />
+            <button className={styles.elementPage} onClick={() => setPageBook((page + 1)%2)}> Next</button> 
+          </div>  
+         }
+        { page === 1 &&
+          <div className={styles.bookPage} >
+            <SecondPageSVG  />
+            
+            <button className={styles.elementPage} onClick={() => setPageBook((page + 1)%2)}>Back</button> 
 
-        <div className={styles.secondPage}>
-          <SecondPageSVG/>
-        </div>      
-      </div>             
+            <div className={styles.boxButtonContes}>
+              <button className={styles.buttonBook}>SpiderMan</button> 
+              <button className={styles.buttonBook}>Frozen</button> 
+              <button className={styles.buttonBook}>Ratatouille</button> 
+              <button className={styles.buttonBook}>Caperucita</button> 
+              <button className={styles.buttonBook}>Blancanieves</button>
+            </div>       
+          </div> }         
+      </div> 
+      
+      
+      
+
+      {/* <Button className={styles.secondPage} >Next</Button> */}
+
       </UniquePageLayout>
     </div>
   )
