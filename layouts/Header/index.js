f6ed6b3d7d7a5bf8a131bff2767357a67d338013
Antonio Marín Rodríguez <amrodriguezjarote@gmail.com>
2022-06-29 19:21:15 +0200

    Responsive Header

--- layouts/Header/index.js
+++ layouts/Header/index.js
@@ -2,14 +2,16 @@ import Navbar from 'components/Navbar'
 import NavBarWood from 'components/SVG/navBar'
 import ShortHeader from 'components/SVG/shortNavBar'
 import styles from './Header.module.css'
+import { useWindowSize } from '../../hooks/screenWidth'
 
 export default function Header () {
+  const size = useWindowSize();
   return (
         <>
             <div className={styles.head}>
                 <div className={styles.headTitle}>
-                  {/* <NavBarWood/> */}
-                  <ShortHeader/>
+                   {size.width > 800 && <NavBarWood/> }
+                  {size.width <= 800 && <ShortHeader/> }   
                 </div>
                 <Navbar></Navbar>
             </div>
