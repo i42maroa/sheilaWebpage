4780cc14b1c24de5a12926bcf32048127aa7fc60
Antonio Marín Rodríguez <amrodriguezjarote@gmail.com>
2022-06-30 16:30:06 +0200

    Add select navbar

--- components/MoiCard/index.js
+++ components/MoiCard/index.js
@@ -6,7 +6,13 @@ export default function MoiCard () {
           <>
             <div className={styles.postContainer}>
               <div className={styles.textContainer}>
-                <p className={styles.text}> Bonjour! soy Sheila y estoy opositando para estoy opositando acony esta es mi historia</p>
+                <p className={styles.text}> 
+                  Bienvenue sur mon site web, je m&apos;appelle Sheila, je suis maitresse de français. 
+                  J&apos;ai créé cette plateforme en ligne pour motiver mes enfants à apprendre une nouvelle langue comme le français. 
+                  Connaître d&apos;autres cultures nous ouvre la porte de la sagesse. 
+                  Malheureusement, avec une seule heure par semaine on doit faire de notre mieux pour que nos élèves apprennent le français, nous avons de pain sur la planche. 
+                  On y va !
+                </p>
               </div>
               <div className={styles.imageContainer}>
                 <img className={styles.image} src="./sheila-portada-final.svg" width={400}/>
